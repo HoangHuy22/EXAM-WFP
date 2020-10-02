a4127950b6f1c44eada1f3afa2702d3a74837a13
Your Name <huy hoang>
2020-10-02 16:40:32 +0700

    nop bai thi

---
+++ EXAM-WFP/Pages/NewtonJson.xaml.cs
@@ -0,0 +1,38 @@
+﻿using EXAM_WFP.Adapters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EXAM_WFP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NewtonJson : Page
+    {
+        public NewtonJson()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string txt = await EmployeeFileHandService.ReadFile("t1907a.txt");
+            TxtBlock.Text = txt;
+        }
+    }
+}
